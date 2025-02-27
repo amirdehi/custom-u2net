83096744a4950b4ed56b7f5b67ad1b695acc2842
Ali Ramezanpour <ali.freelance.amirdehi@gmail.com>
2025-02-27 18:16:16 +0330

    Optimize and refactor
    optimize codes and refactor whitespaces with fixing the workers problem

--- u2net_train.py
+++ u2net_train.py
@@ -1,26 +1,17 @@
-import os
-import torch
-import torchvision
-from torch.autograd import Variable
-import torch.nn as nn
-import torch.nn.functional as F
-
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
-import torch.optim as optim
-import torchvision.transforms as standard_transforms
-
-import numpy as np
 import glob
 import os
 
-from data_loader import Rescale
-from data_loader import RescaleT
-from data_loader import RandomCrop
-from data_loader import ToTensor
-from data_loader import ToTensorLab
-from data_loader import SalObjDataset
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
 
+from data_loader import RandomCrop
+from data_loader import RescaleT
+from data_loader import SalObjDataset
+from data_loader import ToTensorLab
 from model import U2NET
 from model import U2NETP
 
@@ -28,25 +19,27 @@ from model import U2NETP
 
 bce_loss = nn.BCELoss(size_average=True)
 
+
 def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
+    loss0 = bce_loss(d0, labels_v)
+    loss1 = bce_loss(d1, labels_v)
+    loss2 = bce_loss(d2, labels_v)
+    loss3 = bce_loss(d3, labels_v)
+    loss4 = bce_loss(d4, labels_v)
+    loss5 = bce_loss(d5, labels_v)
+    loss6 = bce_loss(d6, labels_v)
 
-	loss0 = bce_loss(d0,labels_v)
-	loss1 = bce_loss(d1,labels_v)
-	loss2 = bce_loss(d2,labels_v)
-	loss3 = bce_loss(d3,labels_v)
-	loss4 = bce_loss(d4,labels_v)
-	loss5 = bce_loss(d5,labels_v)
-	loss6 = bce_loss(d6,labels_v)
+    loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6
+    print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n" % (
+    loss0.data.item(), loss1.data.item(), loss2.data.item(), loss3.data.item(), loss4.data.item(), loss5.data.item(),
+    loss6.data.item()))
 
-	loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6
-	print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n"%(loss0.data.item(),loss1.data.item(),loss2.data.item(),loss3.data.item(),loss4.data.item(),loss5.data.item(),loss6.data.item()))
-
-	return loss0, loss
+    return loss0, loss
 
 
 # ------- 2. set the directory of training dataset --------
 
-model_name = 'u2net'  #'u2netp'
+model_name = 'u2net'  # 'u2netp'
 
 data_dir = os.path.join(os.getcwd(), 'train_data' + os.sep)
 tra_image_dir = os.path.join('im_aug' + os.sep)
@@ -67,15 +60,15 @@ tra_img_name_list = glob.glob(data_dir + tra_image_dir + '*' + image_ext)
 
 tra_lbl_name_list = []
 for img_path in tra_img_name_list:
-	img_name = img_path.split(os.sep)[-1]
+    img_name = img_path.split(os.sep)[-1]
 
-	aaa = img_name.split(".")
-	bbb = aaa[0:-1]
-	imidx = bbb[0]
-	for i in range(1,len(bbb)):
-		imidx = imidx + "." + bbb[i]
+    aaa = img_name.split(".")
+    bbb = aaa[0:-1]
+    imidx = bbb[0]
+    for i in range(1, len(bbb)):
+        imidx = imidx + "." + bbb[i]
 
-	tra_lbl_name_list.append(data_dir + tra_label_dir + imidx + label_ext)
+    tra_lbl_name_list.append(data_dir + tra_label_dir + imidx + label_ext)
 
 print("---")
 print("train images: ", len(tra_img_name_list))
@@ -95,10 +88,10 @@ salobj_dataloader = DataLoader(salobj_dataset, batch_size=batch_size_train, shuf
 
 # ------- 3. define model --------
 # define the net
-if(model_name=='u2net'):
+if model_name == 'u2net':
     net = U2NET(3, 1)
-elif(model_name=='u2netp'):
-    net = U2NETP(3,1)
+elif model_name == 'u2netp':
+    net = U2NETP(3, 1)
 
 if torch.cuda.is_available():
     net.cuda()
@@ -114,15 +107,6 @@ running_loss = 0.0
 running_tar_loss = 0.0
 ite_num4val = 0
 save_frq = 50  # save the model every 2000 iterations
-
-pretrained_model_path = "saved_models/pretrained/u2net.pth"
-
-if os.path.exists(pretrained_model_path):
-    # Load the pre-trained model
-    net.load_state_dict(torch.load(pretrained_model_path))
-    print(f"Pre-trained model loaded from {pretrained_model_path}")
-else:
-    print("No pre-trained model found. Training from scratch.")
 
 for epoch in range(0, epoch_num):
     net.train()
@@ -146,7 +130,7 @@ for epoch in range(0, epoch_num):
         # y zero the parameter gradients
         optimizer.zero_grad()
 
-        # forward + backward + optimize
+        # forward + backward + optimize l
         d0, d1, d2, d3, d4, d5, d6 = net(inputs_v)
         loss2, loss = muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v)
 
@@ -161,11 +145,12 @@ for epoch in range(0, epoch_num):
         del d0, d1, d2, d3, d4, d5, d6, loss2, loss
 
         print("[epoch: %3d/%3d, batch: %5d/%5d, ite: %d] train loss: %3f, tar: %3f " % (
-        epoch + 1, epoch_num, (i + 1) * batch_size_train, train_num, ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
+            epoch + 1, epoch_num, (i + 1) * batch_size_train, train_num, ite_num, running_loss / ite_num4val,
+            running_tar_loss / ite_num4val))
 
         if ite_num % save_frq == 0:
-
-            torch.save(net.state_dict(), model_dir + model_name+"_bce_itr_%d_train_%3f_tar_%3f.pth" % (ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
+            torch.save(net.state_dict(), model_dir + model_name + "_bce_itr_%d_train_%3f_tar_%3f.pth" % (
+            ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
             running_loss = 0.0
             running_tar_loss = 0.0
             net.train()  # resume train

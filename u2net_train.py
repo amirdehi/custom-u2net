7a1140197757c7453b8c4733180c64a1f7a34caa
Ali Ramezanpour <ali.freelance.amirdehi@gmail.com>
2025-02-27 19:18:47 +0330

    fix warning
    fix warning by using not deprecated way

--- u2net_train.py
+++ u2net_train.py
@@ -17,7 +17,7 @@ from model import U2NETP
 
 # ------- 1. define loss function --------
 
-bce_loss = nn.BCELoss(size_average=True)
+bce_loss = nn.BCELoss(reduction='mean')
 
 
 def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
